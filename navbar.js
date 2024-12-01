99b24df3b006075389cb6aa5c5787cfcf6ba9ac1
cartwrightjoseph@ouprice <jacartwright.ok@gmail.com>
2024-12-01 03:36:36 -0600

    Start

---
+++ navbar.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+document.addEventListener("DOMContentLoaded", function () {
+	fetch('navbar.html')
+		.then(response => response.text())
+		.then(data => {
+			document.getElementById('navbar-container').innerHTML = data;
+		});
+});
